716e3d59530c3e43f0500a31f39704906442fcd7
ectionboy <125730248+ectionboy@users.noreply.github.com>
2023-11-09 00:18:14 +0200

    v1

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,55 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
+class Person {
+  constructor(private key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  private tenants: Person[] = [];
+  abstract openDoor(key: Key): void;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Wolcome home");
+    } else {
+      console.log("The doors are closed");
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The doors are open");
+    } else {
+      console.log("The doors are closen. Try again");
+    }
+  }
+}
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
